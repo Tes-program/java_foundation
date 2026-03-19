a3c793108a29817fe6173c33d2f3d094cda74059
Tes-program <teslimodumuyiwa@gmail.com>
2026-03-19 10:01:59 +0100

    [TEST]: Testing stack implementation

--- test/dsa/StackTest.java
+++ test/dsa/StackTest.java
@@ -1,11 +1,66 @@
 package dsa;
 
+import Day3.dsa.Stack;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.*;
+
 public class StackTest {
+    private Stack strings;
+
+    @BeforeEach
+    public void setUp() {
+        strings = new Stack();
+    }
 
     @Test
-    public void nonsenseTest() {
+    @DisplayName("Stack should be empty")
+    public void newStack_isEmptyTest() {
+        assertTrue(strings.isEmpty());
+    }
+
+    @Test
+    public void addElement_stackIsNotEmptyTest() {
+        assertTrue(strings.isEmpty());
+        strings.push("G-strings");
+        assertFalse(strings.isEmpty());
+    }
+
+    @Test
+    public void pushOneElement_popOneElement_StackIsEmptyTest() {
+        strings.push("strings");
+        assertFalse(strings.isEmpty());
+
+        String popped = strings.pop();
+        assertTrue(strings.isEmpty());
+    }
+
+    @Test
+    public void pushOneElement_popOneElement_stackIsNotEmptyTest() {
+        strings.push("G-strings");
+        strings.push("A-strings");
+        assertFalse(strings.isEmpty());
+
+        String popped = strings.pop();
+        assertFalse(strings.isEmpty());
+    }
+
+    @Test
+    public void pushX_popReturnsXTest () {
+        strings.push("G-strings");
+        assertEquals("G-strings", strings.pop());
+    }
+
+    @Test
+    public void pushXYZ_popReturnsXYZTest () {
+        strings.push("G-strings");
+        strings.push("A-strings");
+        strings.push("D-strings");
+        assertEquals("D-strings", strings.pop());
+        assertEquals("A-strings", strings.pop());
+        assertEquals("G-strings", strings.pop());
 
     }
 }
